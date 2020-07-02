48ec57456f61a7cf64251ef1af02da1b2f3d63d5
Blasty1 <ichigoplayer18@gmail.com>
2020-07-02 19:56:05 +0200

    ok

--- GdrDestiny/resources/views/welcome2.blade.php
+++ GdrDestiny/resources/views/welcome2.blade.php
@@ -9,22 +9,34 @@
 <body>
     <section>
         <img id='sfondo' src="/img/imgHomeEsterna/sfondo.png" alt="">
+        
         <div class="title">
             <img src="/img/imgHomeEsterna/titleHome.png" alt="">
         </div>
         <div class="SezioniHome titoloSinistra">
+            <img src="/img/imgHomeEsterna/sxsimbolo.png" class='symbol' alt="">
             <img src="/img/imgHomeEsterna/iscrizione.png" alt="">
         </div>
         <div class="SezioniHome titoloDestra">
+            <img src="/img/imgHomeEsterna/dxtsimbolo.png" class='symbol'alt="">
             <img src="/img/imgHomeEsterna/ambientazione.png" alt="">
         </div>
         <div class="ContainerSinistra">
+            <img src="/img/imgHomeEsterna/regolamento.png" alt="">
             <img src="/img/imgHomeEsterna/sxpg.png" alt="">
         </div>
         <div class="ContainerCentrale">
-            <img src="/img/imgHomeEsterna/centralpg.png" alt="">
+        <div class="circonferenzaBlu">
+            <img src="/img/imgHomeEsterna/circonferenza.png" alt="">
+            </div>
+            <div class="loginButton">
+            <img id='loginText' src="/img/imgHomeEsterna/login.png" alt="">
+            </div>
+            <img id='PersonaggioCentrale' src="/img/imgHomeEsterna/centralpg.png" alt="">
+            
         </div>
         <div class="ContainerDestra">
+            <img src="/img/imgHomeEsterna/credits.png" alt="">
             <img src="/img/imgHomeEsterna/dxpg.png" alt="">
         </div>
     </section>
