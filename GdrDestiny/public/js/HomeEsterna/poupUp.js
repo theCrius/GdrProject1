8cbfe16e2a3f8c7120160e8e1271ed573b5f31c0
Blasty1 <ichigoplayer18@gmail.com>
2020-07-16 23:06:32 +0200

    ok

--- GdrDestiny/public/js/HomeEsterna/poupUp.js
+++ GdrDestiny/public/js/HomeEsterna/poupUp.js
@@ -1,9 +1,10 @@
 class poupUp{
     static createWindowPoupUp(url){
-        let h=750;
-        let w=450;
-        var left = (screen.width/2)-(h/2);
-        var top = (screen.height/2)-(w/2);
+        
+        let h=screen.height*0.50;
+        let w=screen.width*0.60;
+        var left = (screen.width/2)-(w/2);
+        var top = (screen.height/2)-(h/1.5);
         this.popupWindow = window.open(
             url,'popUpWindow','height='+h+',width='+w+',left='+left+',top='+top+',resizable=no,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
     }
