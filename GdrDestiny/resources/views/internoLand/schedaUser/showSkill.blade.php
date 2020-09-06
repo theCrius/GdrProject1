60b10aa07b72cb24d8830f33f9d29a4a61d23d09
Blasty1 <ichigoplayer18@gmail.com>
2020-09-06 11:03:55 +0200

    ok

--- GdrDestiny/resources/views/internoLand/schedaUser/showSkill.blade.php
+++ GdrDestiny/resources/views/internoLand/schedaUser/showSkill.blade.php
@@ -1,18 +1,31 @@
 @extends('../layouts.appModalInterno')
 
 @section('content')
+<div class="abilita">
 <div class="leftBox">
 
 <div class="boxAbilita boxRazza">
-<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBoxLeft'>
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
 </div>
 <div class="boxAbilita boxClasse">
-<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBoxLeft'>
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
 </div>
 <div class="boxAbilita boxEmisfero">
-<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBoxLeft'>
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
 </div>
 </div>
-<div class="rightBox"></div>
+<div class="rightBox">
+<div class="boxSpec boxRazza">
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
+</div>
+<div class="boxSpec boxClasse">
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
+</div>
+<div class="boxSpec boxEmisfero">
+<img src="/img/imgHomeInterna/home/schedaPg/abilità/frameab.png" alt="" class='sfondoBox'>
+</div>
+</div>
 
+</div>
+</div>
 @endsection
