d7893a33926622a834d2da8ebac4cc09e8c1bdef
Blasty1 <ichigoplayer18@gmail.com>
2020-11-04 13:00:45 +0100

    ok

--- GdrDestiny/app/Http/Controllers/UserspecializationController.php
+++ GdrDestiny/app/Http/Controllers/UserspecializationController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class UserspecializationController extends Controller
 {
     public function addSpecs(int $idUser,string $specFrom){
+    
+        //$userSkill=\App\Userskill::select('id')->where('id_skill')
+        //$test=\App\Specialization::where('')
        return view('internoLand.schedaUser.addSpec',[
         'idUser' => $idUser,
         'specs' => $specFrom
--- GdrDestiny/resources/views/internoLand/schedaUser/addSpec.blade.php
+++ GdrDestiny/resources/views/internoLand/schedaUser/addSpec.blade.php
@@ -3,39 +3,46 @@
 <div class="abilitaAdd">
     <div class="titleWithBackground">
     <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/rigatit.png" alt="" id='sfondoTitle'>
-    <div class='titleImg'><img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/sceglispec.png" alt=""></div>
+    <div class='centerToAbsolutePosition'><img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/sceglispec.png" alt="" class='titleHeight'></div>
     </div>
 
     <form action="{{route('storeSpecs',$idUser)}}" method='POST'>
         @csrf
-    <div class="containerWithSfondo">
+    <div class="containerWithBackground">
+        <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/riqspec.png" alt="" id='sfondoTitle'>
+    <div class="centerToAbsolutePosition totalWidth">
         <div class="containerAbilita">
         <div class="leftContainer">
      
       
         @for($i=0;$i < 5; $i++) 
-        <div class="skillToSelect" id='spec'>
-    
-            <img src="/img/imgHomeInterna/home/schedaPg/abilità/addAbilità/sfondoskill.png" class='sfondoRiquadroSkill'  data-id="{{\Crypt::encrypt(}" alt"">
-            <div class="contenitoreText">
-                <div class="title">
-                
-            </div>
-                <div class="description">
+        <div class='riquadroSpec'>
+            <div class="skillToSelect modifyHeWSpec" id='spec'>
+                <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/descrriq.png" class='sfondoRiquadroSkill'  data-id="{{\Crypt::encrypt(}" alt""> 
+                <div class="contenitoreText">
+                    <div class="title">
                     
                 </div>
+                    <div class="description">
+                        
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class='logoSpec'>
+                <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/specvuota.png" class='sfondoRiquadroSkill'> 
+                <div class="contenitoreText">
+                   
+                    </div>
+                </div>
+            </div>
         @endfor
       
     </div>
-    <div class="centerContainer">
-    <img src="/img/imgHomeInterna/home/schedaPg/abilità/addAbilità/ghostcentrale.gif" class='' alt="">
-    </div>
     <div class="rightContainer">
     @for($i=5;$i < 10; $i++)
-        <div class="skillToSelect" id='spec'>
-            <img src="/img/imgHomeInterna/home/schedaPg/abilità/addAbilità/sfondoskill.png" class='sfondoRiquadroSkill'  data-id="{{\Crypt::encrypt(}" alt""> 
+        <div class='riquadroSpec'>
+        <div class="skillToSelect modifyHeWSpec" id='spec'>
+            <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/descrriq.png" class='sfondoRiquadroSkill'  data-id="{{\Crypt::encrypt(}" alt""> 
             <div class="contenitoreText">
                 <div class="title">
                 
@@ -45,16 +52,27 @@
                 </div>
             </div>
         </div>
+        <div class='logoSpec'>
+            <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/specvuota.png" class='sfondoRiquadroSkill'> 
+            <div class="contenitoreText">
+               
+                </div>
+            </div>
+        </div>
         @endfor
     </div>
         </div>
-        <div class="buttonConferma">
-        <button type="submit" id='conferma'><span><img src="/img/imgHomeInterna/home/schedaPg/conferma.png"
-                    alt=""></span></button>
-        
-        
+    </div>
         </div>
-        <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/rigatit.png" alt="" id='sfondoTitle'>
+        <div class="titleWithBackground">
+            <img src="/img/imgHomeInterna/home/schedaPg/Specializzazione/rigatit.png" alt="" id='sfondoTitle'>
+            <div class="centerToAbsolutePosition">
+        <button type="submit" id='conferma'><span><img src="/img/imgHomeInterna/home/schedaPg/conferma.png"
+                    alt="" class='titleHeight'></span></button>
+        </div>
+                    
+        
+        
     </div>
     </form>
 </div>
