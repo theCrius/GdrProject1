a35eb0f6efe293dc231d562106365a887a7992b5
Blasty1 <ichigoplayer18@gmail.com>
2020-11-30 19:12:41 +0100

    ok

--- GdrDestiny/resources/views/internoLand/schedaUser/showEquippedObject.blade.php
+++ GdrDestiny/resources/views/internoLand/schedaUser/showEquippedObject.blade.php
@@ -11,8 +11,33 @@
         </div>
         @endfor
     </div>
-    <div class="center"></div>
-    <div class="right"></div>
+    <div class="center">
+        <div class="top">
+        @for($i=4;$i < 7; $i++)
+            <div class="equipped">
+            <img src="/img/imgHomeInterna/home/schedaPg/ObjectsEquipped/qoggetto.png" alt="" id='riquadroObjectEquipped'>
+
+            </div>
+        @endfor
+        </div>
+        <div class="middle"></div>
+        <div class="bottom">
+        @for($i=7;$i < 10; $i++)
+        <div class="equipped">
+        <img src="/img/imgHomeInterna/home/schedaPg/ObjectsEquipped/qoggetto.png" alt="" id='riquadroObjectEquipped'>
+
+        </div>
+        @endfor
+        </div>
+    </div>
+    <div class="right">
+    @for($i=10;$i < 14; $i++)
+        <div class="equipped">
+        <img src="/img/imgHomeInterna/home/schedaPg/ObjectsEquipped/qoggetto.png" alt="" id='riquadroObjectEquipped'>
+
+        </div>
+        @endfor
+    </div>
 </div>
 </div>
 @endsection
