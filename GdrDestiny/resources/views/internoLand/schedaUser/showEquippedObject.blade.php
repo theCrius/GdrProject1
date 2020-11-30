57f14bd5592fe7ee1b9ee3faa61eab513c3861df
Blasty1 <ichigoplayer18@gmail.com>
2020-11-30 19:00:45 +0100

    ok

--- GdrDestiny/resources/views/internoLand/schedaUser/showEquippedObject.blade.php
+++ GdrDestiny/resources/views/internoLand/schedaUser/showEquippedObject.blade.php
@@ -1,11 +1,14 @@
 @extends('../layouts.appModalInterno')
 @section('content')
 <div class="equipContent">
-<img src="/img/imgHomeInterna/home/schedaPg/background/backsfondobac.png" alt="" id='sfondoEquipped' class='centerToAbsolutePosition'>
-<div class="equip">
+<img src="/img/imgHomeInterna/home/schedaPg/background/backsfondobac.png" alt=""  class='equippedSfondo centerToAbsolutePosition'>
+<div class="equip centerToAbsolutePosition">
     <div class="left">
-        @for($i=0;$i < 4; $i)
-            <img src="/img/imgHomeInterna/home/schedaPg/ObjectsEquipped/qoggetto.png" alt="" id='riquadroObjectEquipped'>
+        @for($i=0;$i < 4; $i++)
+        <div class="equipped">
+        <img src="/img/imgHomeInterna/home/schedaPg/ObjectsEquipped/qoggetto.png" alt="" id='riquadroObjectEquipped'>
+
+        </div>
         @endfor
     </div>
     <div class="center"></div>
